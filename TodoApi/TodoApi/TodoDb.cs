2e0eaaf4b123c13bf3c6a88730746baf4084ccfb
Josué González <60802835+15osvaldo@users.noreply.github.com>
2023-06-06 22:27:48 -0600

    Add files via upload

---
+++ TodoApi/TodoApi/TodoDb.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+namespace TodoApi
+{
+    public class TodoDb : DbContext
+    {
+        // Se define el contrexto de la base de datos 
+        // siendo la clase principal que cordina la funcionalidad de 
+        // Entity Framework para los modelos de datos
+        public TodoDb(DbContextOptions options) 
+            : base(options) { }
+
+        public DbSet<Todo> Todos => Set<Todo>();
+    }
+}
